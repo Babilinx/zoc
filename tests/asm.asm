40774fa5817fa0c9045f10b56d769412229aebcb
babilinx <babilinx.evx1o@simplelogin.com>
2025-01-04 21:34:59 +0100

    feat(asm): add scratch for if, elif and else

--- tests/asm.asm
+++ tests/asm.asm
@@ -85,6 +85,60 @@ macro zoc_add_qword {
 }
 
 
+; id: the if number identifier
+; --
+; none
+; TODO: use byte instead of qword
+macro zoc_if id* {
+        zoc_pop_qword rax
+        cmpq rax, 1
+        jne if_end_#id
+}
+
+macro zoc_if_else id*, else_id {
+        zoc_pop_qword rax
+        cmp rax, 1
+        if else_id eq
+            jne else_#id
+        else
+            jne else_#id_#else_id
+        end if
+}
+
+; id: the if number identifier
+; else_id: the else number identifier
+;    if else_id is null, jump to the end on false
+; stack: pop a qword
+; --
+; none
+macro zoc_elif id*, else_id {
+        zoc_pop_qword rax
+        cmp rax, 1
+        if else_id eq ; empty
+            jne if_end_#id
+        else
+            jne else_#id_#else_id
+        end if
+}
+
+; id: the if number identifier
+; else_id: the else number identifier
+; --
+; none
+macro zoc_else id*, else_id {
+        jmp if_end_#id
+        if else_id eq 
+else_#id:
+        else
+else_#id_#else_id:
+        end if
+}
+
+macro zoc_if_end id* {
+if_end_#id:
+}
+
+
 section '.text' executable
 public _start
 _start:
@@ -128,23 +182,73 @@ _start:
 ; void
 ; --
 ; usize
-zoc_fn 0        
-        ; 69 usize
-        mov rax, 69
-        zoc_push_qword rax
-        ; 37 usize
-        mov rax, 37
-        zoc_push_qword rax
+zoc_fn 0
+        ; call ifTest
+        zoc_call_fn 1, 8
+        ; call ifTest2
+        zoc_call_fn 2, 8
         ; + usize
         zoc_add_qword
 zoc_ret_qword
 
+; ifTest2
+; none
+; --
+; usize
+zoc_fn 2
+        ; 0 usize
+        mov rax, 0
+        zoc_push_qword rax
+        ; if
+        zoc_if_else 1
+            mov rax, 20
+            zoc_push_qword rax
+        zoc_else 1
+            mov rax, 40
+            zoc_push_qword rax
+        zoc_if_end 1
+        
+zoc_ret_qword
 
-;; add
-; usize usize
+
+;; ifTest
+; none
 ; --
 ; usize
 zoc_fn 1
-
+        ; 0 usize
+        mov rax, 1
+        zoc_push_qword rax
+        ; if
+        zoc_if_else 0, 0
+                ; 37 usize
+                mov rax, 37
+                zoc_push_qword rax
+        ; else
+        zoc_else 0, 0
+                ; 0 usize
+                mov rax, 0
+                zoc_push_qword rax
+        ; elif
+        zoc_elif 0, 1
+                ; 10 usize
+                mov rax, 10
+                zoc_push_qword rax
+        ; else
+        zoc_else 0, 1
+                ; 1 usize
+                mov rax, 0
+                zoc_push_qword rax
+        ; elif
+        zoc_elif 0, 2
+                ; 100 usize
+                mov rax, 100
+                zoc_push_qword rax
+        zoc_else 0, 2
+                ; 0 usize
+                mov rax, 10
+                zoc_push_qword rax
+        ; if_end
+        zoc_if_end 0
         
 zoc_ret_qword
